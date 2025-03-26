e378f1837bd0d5a727b1c740bde42aa27f4accbe
caioguarnieri <caioguarniele@hotmail.com>
2025-03-25 22:10:58 -0300

    Created the option inside the select and it shows

---
+++ script.js
@@ -0,0 +1,30 @@
+function adicionar(){
+
+let userNumber = Number(window.document.getElementById('fnum').value)
+let userNumberedata = userNumber;
+let validationResult = '';
+let numeroAdicionado = document.createElement("option");
+let textoDoNumero = document.createTextNode(`O número ${userNumberedata} foi adicionado`);
+numeroAdicionado.appendChild(textoDoNumero);
+let teste = document.getElementById('flista').appendChild(numeroAdicionado);
+
+console.log(teste);
+
+
+
+
+function validar0a100(userNumberedata){
+    if (userNumberedata > -1 && userNumberedata < 101){
+    validationResult = true;
+}else{
+   validationResult = false ;
+};
+
+
+
+
+
+}
+console.log(userNumber);
+console.log(validationResult);
+}
