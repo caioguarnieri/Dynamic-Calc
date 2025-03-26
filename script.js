f96e490a9061f9e79efaa45120ca82d5bdb65a28
caioguarnieri <caioguarniele@hotmail.com>
2025-03-26 20:32:37 -0300

    All done

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 let numbers = [];
-function adicionar(){
 
+function adicionar(){
 let userNumber = Number(window.document.getElementById('fnum').value)
 let userNumberedata = userNumber;
-
-
-//let numeroAdicionado = document.createElement("option");
-//let textoDoNumero = document.createTextNode(`O número ${userNumberedata} foi adicionado`);
-//numeroAdicionado.appendChild(textoDoNumero);
-//let teste = document.getElementById('flista').appendChild(numeroAdicionado);
-
 if (validar0a100(userNumberedata)){
 console.log('DENTRO');
 numbers.push(userNumberedata);
@@ -19,14 +12,10 @@ numeroAdicionado.appendChild(textoDoNumero);
 let teste = document.getElementById('flista').appendChild(numeroAdicionado);
 
 userNumber.value = '';
-  
-    console.log(numbers);
-    console.log(userNumber);
 }else{
     window.alert('O NUMERO JA EXISTE NA LISTA OU ESTÁ FORA DE 0 Á 100')
     
 }
-
 function validar0a100(n){
     if (n > -1 && n < 101){
     return  true;
@@ -34,6 +23,21 @@ function validar0a100(n){
    return false ;
 };
 
-
  }
+}
+
+function finalizar(){
+    let answer = window.document.getElementById('res');
+    let totalNumbers = numbers.length;
+    let biggerNumber = Number(Math.max(...numbers));
+    let smallestNumber = Number(Math.min(...numbers));
+    let soma1 = 0;
+    let somar = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, soma1,);
+    let average = somar / totalNumbers;
+
+    answer.innerHTML = `<p>Ao todo temos <strong>${totalNumbers}</strong> numeros cadastrados</p>
+                        <p>O maior número é <strong>${biggerNumber}</strong> </p> 
+                        <p>O menor número é <strong>${smallestNumber}</strong> </p>     
+                        <p>A soma de todos os números é <strong>${somar}</strong> </p>
+                        <p>A soma de todos os números é <strong>${average}</strong> </p> `
 }
